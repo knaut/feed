2e99e60a395000a91255fc22d87fc8f40719ea76
_knaut <info@knaut.net>
2018-12-28 02:52:41 -0800

    added grommet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+// IMPORTS
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+// COMPONENTS
+import AppGrid from './components/AppGrid.jsx';
 
 // STYLES
 import styles from './styles';
@@ -7,26 +10,27 @@ import styles from './styles';
 class App extends Component {
   render() {
     console.log(styles)
-    return (
-      <div 
-        style={{
-          background: styles.colors.darks.purple,
-          ...styles.app.container
-        }}
-      >
-        <header>
-          <h1
-            style={{
-              color: styles.colors.pastels.purple
-            }}
-          >
-            welcome to <strong style={{
-              color: styles.colors.primaries.purple
-            }}>feed</strong>.
-          </h1>
-        </header>
-      </div>
-    );
+    return (<AppGrid/>)
+    // return (
+    //   <div 
+    //     style={{
+    //       background: styles.colors.darks.purple,
+    //       ...styles.app.container
+    //     }}
+    //   >
+    //     <header>
+    //       <h1
+    //         style={{
+    //           color: styles.colors.pastels.purple
+    //         }}
+    //       >
+    //         welcome to <strong style={{
+    //           color: styles.colors.primaries.purple
+    //         }}>feed</strong>.
+    //       </h1>
+    //     </header>
+    //   </div>
+    // );
   }
 }
 
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// STYLES
+import styles from '../styles';
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div>
+        <a>Sign </a>
+      </div>
+    );
+  }
+}
