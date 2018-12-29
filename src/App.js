cc1b5d7c491e7d0f75b23c5b14460d2d74a9296d
_knaut <info@knaut.net>
2018-12-28 22:29:52 -0800

    added grommet

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 // IMPORTS
 import React, { Component } from 'react';
-
-// COMPONENTS
-import AppGrid from './components/AppGrid.jsx';
+import { Grommet, Box, Button } from 'grommet';
+import { grommet, dark } from 'grommet/themes';
+import { Login } from "grommet-icons";
 
 // STYLES
 import styles from './styles';
 
 class App extends Component {
   render() {
-    console.log(styles)
     return (
-      <div 
-        style={{
-          background: styles.colors.darks.purple,
-          ...styles.app.container
-        }}
-      >
-        <header>
-          <h1
-            style={{
-              color: styles.colors.pastels.purple
-            }}
-          >
-            welcome to <strong style={{
-              color: styles.colors.primaries.purple
-            }}>feed</strong>.
-          </h1>
-        </header>
-      </div>
+      <Grommet theme={grommet}>
+        <div 
+          style={{
+            background: styles.colors.darks.purple,
+            ...styles.app.container
+          }}
+        >
+        <Box pad={{ horizontal: 'xlarge', vertical: 'large' }}>
+          <Box>
+            <header>
+              <h1
+                style={{
+                  color: styles.colors.pastels.purple
+                }}
+              >
+                welcome to <strong style={{
+                  color: styles.colors.primaries.purple
+                }}>feed</strong>.
+              </h1>
+              <p style={{color: styles.colors.pastels.purple}}>feed is a decentralized social network powered by blockchain technology.</p>
+
+              <Box align="center" pad="large" gap="small">
+                <Button icon={<Login />} label="Sign in with your Blockstack ID" onClick={() => {}} primary />
+              </Box>
+
+            </header>
+          </Box>
+        </Box>
+        </div>
+      </Grommet>
     );
   }
 }
