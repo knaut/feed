77ab691d4aac0ecb3761d815599d09c93edae00d
_knaut <info@knaut.net>
2018-12-29 20:53:11 -0800

    grid changes to feed page

--- src/screens/Feed.jsx
+++ src/screens/Feed.jsx
@@ -1,7 +1,7 @@
 // IMPORTS
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grommet, Box, Button } from 'grommet';
+import { Grommet, Box, Button, Grid } from 'grommet';
 import { grommet, dark } from 'grommet/themes';
 import { Login } from "grommet-icons";
 
@@ -23,6 +23,56 @@ class Feed extends Component {
             justifyContent: 'start'
           }}
         >
+          <Grid
+            fill
+            areas={[
+              { name: 'left', start: [0, 0], end: [0, 0] },
+              { name: 'main', start: [1, 0], end: [1, 0] },
+              { name: 'right', start: [2, 0], end: [2, 0] }
+            ]}
+            columns={['xsmall', 'flex', 'xsmall']}
+            rows={['flex']}
+            gap='small'
+          >
+            <Box gridArea='left'>
+              <AddPostButton/>
+            </Box>
+            <Box gridArea='main'>
+
+              <Box align="center" pad="medium">
+                <Box
+                  pad='medium'
+                  gap='small'
+                  round
+                  style={{
+                    background: 'white',
+                    width: '100%',
+                    maxWidth: '800px'
+                  }}
+                >
+                  This is add post
+                </Box>
+              </Box>
+
+            </Box>
+            <Box gridArea='right'>
+              <MyProfileButton/>
+            </Box>
+          </Grid>
+        </div>
+      </Grommet>
+    );
+  }
+}
+
+/*
+<div 
+          style={{
+            background: styles.colors.darks.purple,
+            ...styles.app.container,
+            justifyContent: 'start'
+          }}
+        >
           <Box
             tag='header'
             justify='between'
@@ -30,12 +80,21 @@ class Feed extends Component {
             direction='row'
           >
             <AddPostButton/>
+            <Box align="center" pad="medium">
+              <Box
+                pad='large'
+                gap='small'
+                round
+                style={{
+                  background: 'white'
+                }}
+              >
+                This is add post
+              </Box>
+            </Box>
             <MyProfileButton/>
           </Box>
         </div>
-      </Grommet>
-    );
-  }
-}
+*/
 
 export default Feed;
