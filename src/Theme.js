be811fe94fca918ec03a28082c98441a7f361aed
_knaut <info@knaut.net>
2019-06-19 01:06:03 -0700

    moved color objects into custom theme, theme story

--- src/Theme.js
+++ src/Theme.js
@@ -10,15 +10,37 @@ import merge from 'deepmerge'
 const feed = merge(grommet, {
   global: {
     colors: {
-      purple: '#7A2EE8'
+      // primaries
+      yellow: '#FFFA21',
+      orange: '#E8901E',
+      red: '#FF2E3D',
+      purple: '#7A2EE8',
+      cyan: '#2EB8FF',
+      // pastel
+      yellowPastel: '#FBFFBC',
+      orangePastel: '#E8D0AB',
+      redPastel: '#FFCAC8',
+      purplePastel: '#D3BBE8',
+      cyanPastel: '#C8E7FF',
+      // dark
+      yellowDark: '#373B21',
+      orangeDark: '#241E14',
+      redDark: '#3B2524',
+      purpleDark: '#1E1624',
+      cyanDark: '#242F3B',
+      // neutrals
+      light: '#ECEFFF',
+      gray1: '#98A1AD',
+      gray2: '#636870',
+      gray3: '#393E47',
+      dark: '#16181D'
     }
   }
 })
 
-console.log(feed)
-
 const Theme = ({ children }) => (
   <Grommet
+    full
     theme={feed}
   >
     { children }
---
+++ stories/Theme.stories.js
@@ -0,0 +1,22 @@
+// IMPORTS
+import React from 'react'
+
+// STORYBOOK UTILS
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import StoryRouter from 'storybook-react-router';
+
+// COMPONENTS
+import {
+  Box
+} from 'grommet'
+
+import Theme from '../src/Theme'
+
+storiesOf('Theme', module)
+  .add('Box Test', () => (
+    <Theme>
+      <Box fill background='light'>Test</Box>
+    </Theme>
+  ))
