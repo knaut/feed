bfe7344e209d64232cbec1c395cb2fa713386231
_knaut <info@knaut.net>
2019-06-29 16:26:50 -0700

    start step component

--- src/screens/Index.js
+++ src/screens/Index.js
@@ -16,6 +16,31 @@ import SignIn from '../components/SignIn';
 // UTILS
 import css from '@emotion/css'
 
+const StartStep = ({ color, number, children }) => (
+  <Box
+    pad={{left: 'large', right: 'large', bottom: 'medium'}}
+    direction='row'
+    justify='center'
+  >
+    <Box
+      round='full'
+      width='small'
+      height='small'
+      border={{
+        size: 'large',
+        color: 'light-6'
+      }}
+      justify='center'
+      align='center'
+    >
+      <Heading color={color} level={1} size='large'>{ number }</Heading>
+    </Box>
+    <Box width='large' justify='center' pad='medium'>
+      { children }
+    </Box>
+  </Box>
+)
+
 class Index extends Component {
   render() {
     return (
@@ -44,7 +69,7 @@ class Index extends Component {
           </Heading>
           <Box>
             <Text size='xlarge'>
-              In <Text size='xlarge' color='brand' css={css`font-weight: bold;`}>feed</Text>, your social media data is saved and encrypted on the blockchain. <span css={css`font-weight: bold;`}>You</span> own your data. There is no central database. There are  <Text size='large' weight='bold'>no ads</Text>. There is just you, your content, and the people you connect with.
+              In <Text size='xlarge' color='brand' css={css`font-weight: bold;`}>feed</Text>, your social media data is saved and encrypted on the blockchain. <span css={css`font-weight: bold;`}>You</span> own your data. There is no central database. There are <span css={css`font-weight: bold;`}>no ads</span>. There is just you, your content, and the people you connect with.
             </Text>
           </Box>
           <Box>
@@ -52,86 +77,19 @@ class Index extends Component {
               Here's how to start:
             </Heading>
           </Box>
-          <Box
-            pad={{left: 'large', right: 'large', bottom: 'medium'}}
-            direction='row'
-            justify='center'
-          >
-            <Box
-              round='full'
-              width='small'
-              height='small'
-              border={{
-                size: 'large',
-                color: 'light-6'
-              }}
-              justify='center'
-              align='center'
-            >
-              <Heading color='brand' level={1} size='large'>1</Heading>
-            </Box>
-            <Box width='large' justify='center' pad='medium'>
-              <Text size='large'>Sign up for an ID from Blockstack. Choose a wise password and 12-word combination. <Text size='large' weight='bold'>Treat this ID as your passport to <Text size='large' color='brand' weight='bold'>feed</Text>.</Text></Text>
-            </Box>
-          </Box>
-          <Box
-            pad={{left: 'large', right: 'large', bottom: 'medium'}}
-            direction='row'
-            justify='center'
-          >
-            <Box
-              round='full'
-              width='small'
-              height='small'
-              border={{
-                size: 'large',
-                color: 'light-6'
-              }}
-              justify='center'
-              align='center'
-            >
-              <Heading color='neutral-3' level={1} size='large'>2</Heading>
-            </Box>
-            <Box width='large' justify='center' pad='medium'>
-              <Text size='large'>Sign into <Text size='large' weight='bold' color='brand'>feed</Text> for the first time.</Text>
-            </Box>
-          </Box>
-          <Box
-            pad={{left: 'large', right: 'large', bottom: 'medium'}}
-            direction='row'
-            justify='center'
-          >
-            <Box
-              round='full'
-              width='small'
-              height='small'
-              border={{
-                size: 'large',
-                color: 'light-6'
-              }}
-              justify='center'
-              align='center'
-            >
-              <Heading color='status-ok' level={1} size='large'>3</Heading>
-            </Box>
-            <Box width='large' justify='center' pad='medium'>
-              <Text size='large'>Create your first post. Posts are limited to <Text size='large' weight='bold'>500 characters</Text>. Every post has a <Text size='large' weight='bold'>permalink</Text> that can be shared easily.</Text>
-            </Box>
-          </Box>
+          <StartStep color='brand' number={1}>
+            <Text size='large'>Sign up for an ID from Blockstack. Choose a wise password and 12-word combination. <Text size='large' weight='bold'>Treat this ID as your passport to <Text size='large' color='brand' weight='bold'>feed</Text>.</Text></Text>
+          </StartStep>
+          <StartStep color='neutral-3' number={2}>
+            <Text size='large'>Sign into <Text size='large' weight='bold' color='brand'>feed</Text> for the first time.</Text>
+          </StartStep>
+          <StartStep color='status-ok' number={3}>
+            <Text size='large'>Create your first post. Posts can be <Text size='large' weight='bold'>500 characters</Text> long. Every post has a <Text size='large' weight='bold'>permalink</Text> that can be shared easily.</Text>
+          </StartStep>
         </Box>
       </React.Fragment>
     );
   }
 }
-
-/*
-<Box fill='horizontal' direction='row-responsive' justify='evenly'>
-            <Box>
-              <Heading level={5}>
-                Feed was created as an antidote to modern social media platforms. Users are not products. We are human beings—and social media should serve our needs, not the other way around.
-              </Heading>
-            </Box>
-          </Box>
-*/
 
 export default Index;
