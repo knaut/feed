6bdf97c7256251b8dd2d420203ff29a952a53ab1
_knaut <info@knaut.net>
2019-07-06 14:31:23 -0700

    include build in production

---
+++ build/precache-manifest.7f08efd296f77016e035b346ca1375f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3941261baa625a6e619",
+    "url": "/static/js/main.e3941261.chunk.js"
+  },
+  {
+    "revision": "909e80faa81850fb7019",
+    "url": "/static/js/1.909e80fa.chunk.js"
+  },
+  {
+    "revision": "e3941261baa625a6e619",
+    "url": "/static/css/main.c205fefa.chunk.css"
+  },
+  {
+    "revision": "7c5e768b58d9a48f15984952764fa759",
+    "url": "/index.html"
+  }
+];
