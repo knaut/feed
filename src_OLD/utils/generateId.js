6219cf2e85489d103617b7be4b423dbcbf989a69
_knaut <info@knaut.net>
2019-07-13 18:25:12 -0700

    removed all old src files

--- src_OLD/utils/generateId.js
+++
@@ -1,8 +0,0 @@
-import generate from 'nanoid/generate'
-
-export default function id () {
-  const idChars = `123456789abcdefhijklmnopqrstuvwxyz`
-  const idLimit = 20
-
-  return generate(idChars, idLimit)
-};
--- src_OLD/utils/toType.js
+++
@@ -1,5 +0,0 @@
-export default function (obj) {
-  // better type checking
-  // https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
-  return ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1].toLowerCase()
-}
