4d292c32e966b875a51e579957e31e3fb5b60b6a
_knaut <info@knaut.net>
2019-08-04 15:13:12 -0700

    fixed preserving whitespace in status cards

--- src/components/Post/Card/Text.js
+++ src/components/Post/Card/Text.js
@@ -9,13 +9,15 @@ export default (props) => {
   const {
     text
   } = props
-
+  
   return (
     <div css={css`
       whitespace: pre-wrap;
       overflow-wrap: break-word;
     `}>
-      {text}
+      {text.split('\n').map((item, key) => {
+        return <span key={key}>{item}<br/></span>
+      })}
     </div>
   )
 }
