16746a8a72fbe90b6e97a8c0b6571c3e42986d49
_knaut <info@knaut.net>
2019-08-04 16:01:09 -0700

    links automatically link with linkify and anchor styles

--- src/components/Post/Card/Text.js
+++ src/components/Post/Card/Text.js
@@ -2,22 +2,45 @@
 import React, { Component } from 'react'
 import Moment from 'moment'
 
+// COMPONENTS
+import Linkify from 'react-linkify'
+import { feed } from '../../../Theme'
+
 // STYLES
 import css from '@emotion/css'
+
+const componentDecorator = (href, text, key) => {
+  return (
+    <a href={href} key={key} target="_blank"
+      css={css`
+        color: ${feed.global.colors.purple};
+        font-weight: bold;
+        transition: all 0.2s ease-in-out;
+        &:hover {
+          color: ${feed.global.colors.cyan};
+        }
+      `}
+    >
+      {text}
+    </a>
+  )
+};
 
 export default (props) => {
   const {
     text
   } = props
-  
+
   return (
-    <div css={css`
-      whitespace: pre-wrap;
-      overflow-wrap: break-word;
-    `}>
-      {text.split('\n').map((item, key) => {
-        return <span key={key}>{item}<br/></span>
-      })}
-    </div>
+    <Linkify componentDecorator={componentDecorator}>
+      <div css={css`
+        whitespace: pre-wrap;
+        overflow-wrap: break-word;
+      `}>
+        {text.split('\n').map((item, key) => {
+          return <span key={key}>{item}<br/></span>
+        })}
+      </div>
+    </Linkify>
   )
 }
